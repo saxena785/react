e0c45af8a1d82a31cbcaa26c24170f71482a0fda
saurabh saxena <saurabhsaxena072@gmail.com>
2020-09-08 16:51:30 +0530

    Fisrt Commit for react2

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Card from "./Cards";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import "./index.css";
+import { BrowserRouter } from 'react-router-dom';
 
-  
 ReactDOM.render(
-                <>
-               <Card 
-               imgsrc = "https://wallpapercave.com/wp/wp4056410.jpg" title ="The Netflix Series" sName="Dark" 
+   <>
+  <BrowserRouter>
+   <App />
+ </BrowserRouter>
+ </>,
+ document.getElementById("root")
+ );
 
-               />
-                 <Card
-                  imgsrc = "https://image.shutterstock.com/image-photo/benon-france-january-21-2018-600w-1008073462.jpg" title ="The Netflix Series" sName="extracurrcular"
-
-                  />
-                  <Card 
-                  imgsrc = "https://image.shutterstock.com/image-photo/watching-series-cup-tea-just-600w-771067420.jpg" title ="The Netflix Series" sName="Stranger Things"
-
-                  />
-                  </>,
-  
-  document.getElementById('root')
-);
